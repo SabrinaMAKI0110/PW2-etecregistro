797e718b17069b630ac1cf998781d0be1c021704
Maki <makisabrina999@gmail.com>
2026-09-18 16:36:36 -0300

    Em progresso

--- Aula09-manipulacao-de-forms-javascript-exercicio/calculadora-javascript/script.js
+++ Aula09-manipulacao-de-forms-javascript-exercicio/calculadora-javascript/script.js
@@ -1,3 +1,40 @@
 // JavScript
 
 const Num1 = document.querySelector('input#Num1');
+const Num2 = document.querySelector('input#Num2');
+
+const resultado = document.querySelector('div#resultado');
+
+function Somar(){
+    const n1 = Number(Num1.value);
+    const n2 = Number(Num2.value);
+    const soma = n1 + n2;
+    resultado.innerHTML = `<strong>${soma}</strong>`;
+}
+
+function Subtrair(){
+    const n1 = Number(Num1.value);
+    const n2 = Number(Num2.value);
+    const subtracao = n1 - n2;
+    resultado.innerHTML = `<strong>${subtracao}</strong>`;
+}
+
+function Multiplicar(){
+    const n1 = Number(Num1.value);
+    const n2 = Number(Num2.value);
+    const multiplicacao = n1 * n2;
+    resultado.innerHTML = `<strong>${multiplicacao}</strong>`;
+}
+
+function Dividir(){
+    const n1 = Number(Num1.value);
+    const n2 = Number(Num2.value);
+    const divisao = n1 / n2;
+    resultado.innerHTML = `<strong>${divisao}</strong>`;
+}
+
+function limpar(){
+    Num1.value = "";
+    Num2.value = "";
+    resultado.innerHTML = "";
+}
